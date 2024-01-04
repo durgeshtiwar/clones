451afa61b6d9e2c58a07f4bceb1dd7e1a92bb73b
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2024-01-04 22:29:32 +0530

    strings

--- JS Assignment 1/script 1.2.js
+++ JS Assignment 1/script 1.2.js
@@ -1,3 +1,10 @@
-let temperature = prompt("Enter any temperture");
-let far = (9*temperature)/5+32;
-console.log(temperature,"c is ",far," F");
+// let temperature = prompt("Enter any temperture");
+// let far = (9*temperature)/5+32;
+// console.log(temperature,"c is ",far," F");
+
+// let str="icecream";
+// console.log(str.slice(0,4));
+
+let name = prompt("Enter your name");
+let username = "@"+`${name}`+`${name.length}`;
+console.log(username); 
