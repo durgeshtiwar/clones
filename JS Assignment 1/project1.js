a98059253723b8dd819875c9eccc6175046fc31a
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2024-01-08 23:59:02 +0530

    winner section

---
+++ JS Assignment 1/project1.js
@@ -0,0 +1,34 @@
+let boxes = document.querySelectorAll(".box");
+let resetbtn = document.querySelector(".reset"); 
+let true0 = true;
+const winpatterns = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8],
+];
+
+boxes.forEach((box)=> {
+    box.addEventListener("click",()=>{
+        console.log("box was clicked");
+        if (true0) {
+            box.innerHTML = "0";
+            true0 = false;
+        } else {
+            box.innerHTML = "x";
+            true0 = true;
+        }
+        box.disabled = true;
+        chekwinner();
+    })
+});
+
+let chekwinner = ()=>{
+    for(pattern of winpatterns){
+        console.log(boxes[pattern[0]], boxes[pattern[1]], boxes[pattern[2]]);
+    }
+}
